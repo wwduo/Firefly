333fb23c7c544994366da2b573234bbdeb2a73eb
wwduo <wangw1741@gmail.com>
2026-06-08 16:13:53 +0800

    修gai

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -26,30 +26,30 @@ export const friendsConfig: FriendLink[] = [
 		title: "xizesha",
 		imgurl:
 			"https://tu.xizesha.com/file/1780903256409_shark11.avif",
-		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
+		desc: "You can 't be mad at me… I' m cute!",
 		siteurl: "https://blog.xizesha.com",
 		tags: ["Blog"],
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
-	{
-		title: "Firefly Docs",
-		imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-		desc: "Firefly主题模板文档",
-		siteurl: "https://docs-firefly.cuteleaf.cn",
-		tags: ["Docs"],
-		weight: 9,
-		enabled: true,
-	},
-	{
-		title: "Astro",
-		imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-		desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
-		siteurl: "https://github.com/withastro/astro",
-		tags: ["Framework"],
-		weight: 8,
-		enabled: true,
-	},
+	// {
+	// 	title: "Firefly Docs",
+	// 	imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
+	// 	desc: "Firefly主题模板文档",
+	// 	siteurl: "https://docs-firefly.cuteleaf.cn",
+	// 	tags: ["Docs"],
+	// 	weight: 9,
+	// 	enabled: true,
+	// },
+	// {
+	// 	title: "Astro",
+	// 	imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
+	// 	desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
+	// 	siteurl: "https://github.com/withastro/astro",
+	// 	tags: ["Framework"],
+	// 	weight: 8,
+	// 	enabled: true,
+	// },
 ];
 
 // 获取启用的友链并进行排序
